73d950ed626546ae4a6bf15f62de1408985a74c4
zoghiat <zoghiat@gmail.com>
2026-06-22 20:13:43 +0330

    new obj

---
+++ Game/ScoutEnemy.cs
@@ -0,0 +1,26 @@
+namespace SpaceShooterFinalWithSounds.Game;
+
+public class ScoutEnemy : Enemy
+{
+    private readonly float startX;
+    private float time;
+
+    public ScoutEnemy(float x, float y, int wave)
+        : base(x, y, 34, 34, 20 + wave * 2, 2.7f + wave * 0.20f, 80, 35, 8)
+    {
+        startX = x;
+    }
+
+    public override void Move(Player player, float deltaTime)
+    {
+        time += deltaTime * 5f;
+        Y += Speed * deltaTime * 60f;
+        X = startX + MathF.Sin(time) * 55f;
+    }
+
+    public override void Draw(Graphics g)
+    {
+        g.FillEllipse(Brushes.Yellow, X, Y, Width, Height);
+        g.DrawEllipse(Pens.White, X, Y, Width, Height);
+    }
+}
